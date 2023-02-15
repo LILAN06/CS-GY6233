42f83292cc1720cd5f968b262345599e8c554af7
LILAN06 <125482509+LILAN06@users.noreply.github.com>
2023-02-15 11:26:30 -0500

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -3,18 +3,18 @@
 #include <errno.h>
 #include <limits.h>
 #include <stdlib.h>
-
 int main (int argc, char *argv[0]) {
     int yearEntered = atoi(argv[1]);
-    if ( yearEntered % 4 == 0) { 
+    if ( yearEntered % 4 == 0) {
         if ( yearEntered % 100 != 0 || yearEntered % 400 == 0)
-            printf("%d was a year", yearEntered);
+            printf("%d was a leap year\n", yearEntered);
         else
-            printf("%d was not a  year", yearEntered);
+            printf("%d was not a leap year\n", yearEntered);
     }
     else {
-        printf("%d was not a  year\n", yearEntered); 
+        printf("%d was not a leap year\n", yearEntered); 
     }
-    printf("%d\n", yearEntered);    
+    printf("%d\n", yearEntered);
+   
 return 0;
-}  
+}
